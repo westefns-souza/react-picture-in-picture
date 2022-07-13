4a5aaa2bc729e52908362358e014a62b7e76a7fb
westefns-souza <westefns@outlook.com.br>
2022-07-12 22:27:58 -0300

    salvando teste de picture in picture com react

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const video = useRef<HTMLVideoElement>(null);
+  
+  function handleClick() {
+    video.current?.requestPictureInPicture();
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,7 +16,10 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
+
+        <video ref={video} width={300} height={300} controls src="https://cdn.arnellebalane.com/videos/original-video.mp4"></video>
+        
+        <a  
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
@@ -18,6 +27,13 @@ function App() {
         >
           Learn React
         </a>
+
+        <button type='button'  
+          className="App-link"
+          onClick={handleClick}
+        >
+          Picture in Picture
+        </button>         
       </header>
     </div>
   );
